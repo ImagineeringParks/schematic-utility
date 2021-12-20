98e9c2f3d466b3817a855dd8dc91562656eff773
Sarah Myerson <myersonsarah1999@gmail.com>
2021-12-20 11:44:36 -0500

    Added commands

--- src/com/sm10259/su/Utils.java
+++ src/com/sm10259/su/Utils.java
@@ -8,8 +8,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Utils extends JavaPlugin
 {
-	public boolean checkPerms(CommandSender sender, String label)
+	/*
+	 * Given the argument, checks if the
+	 * sender has sufficient permissions
+	 */
+	public boolean checkPerms(CommandSender sender, String arg0)
 	{
+		// Use load and list commands
+		if(arg0.equalsIgnoreCase("load"))
+			if(sender.isOp() || sender.hasPermission("schematics.admin") || sender.hasPermission("schematics.load"))
+				return true;
+		
+		// Reload the config
+		if(arg0.equalsIgnoreCase("reload"))
+			if(sender.isOp() || sender.hasPermission("schematics.admin"))
+				return true;
+		
+		// Use the help command
+		if(arg0.equalsIgnoreCase("help"))
+			if(sender.isOp() || sender.hasPermission("schematics.admin") || sender.hasPermission("schematics.load"))
+				return true;
+		
 		return false;
 	}
 	
@@ -20,13 +39,32 @@ public class Utils extends JavaPlugin
 	private String getSchemDir()
 	{
 		Plugin worldEdit = this.getServer().getPluginManager().getPlugin("WorldEdit");
-        String directory = worldEdit.getDataFolder() + File.separator + "schematics";
-        
-        File schemDir = new File(directory);
-        
-        // If schematic folder does not exist, create it
-        if (!schemDir.exists())
-        	schemDir.mkdirs();
-        return directory;
-    }
+		String directory = worldEdit.getDataFolder() + File.separator + "schematics";
+		
+		File schemDir = new File(directory);
+		
+		// If schematic folder does not exist, create it
+		if (!schemDir.exists())
+			schemDir.mkdirs();
+		return directory;
+	}
+	
+	/*
+	 * Check if a string is null or empty. 
+	 * Returns false if it contains values.
+	 */
+	public static boolean isNullEmpty(String str)
+	{
+	    // Check if string is null
+	    if (str == null)
+	      return true;
+
+	    // Check if string is empty
+	    else if(str.isEmpty())
+	      return true;
+	    
+	    // String may contain values
+	    else
+	      return false;
+	  }
 }
--- src/com/sm10259/su/commands/Commands.java
+++ src/com/sm10259/su/commands/Commands.java
@@ -1,5 +1,6 @@
 package com.sm10259.su.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,18 +16,82 @@ public class Commands extends Utils implements CommandExecutor
     }
     
     @Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	public boolean onCommand(CommandSender sender, Command command, String alias, String[] args)
 	{
-    	label = label.toLowerCase();
-    	
     	/*
     	 * Base command: schematic
     	 * Alias: [schem, su]
     	 */
-    	if(args.length > 0 && checkPerms(sender, args[0]))
+    	
+    	// No arguments given
+    	if(alias.equalsIgnoreCase("schematic") && args.length < 1)
     	{
+    		sender.sendMessage(ChatColor.GRAY + "- " + ChatColor.RED + "Too few arguments. View commands: /schematic help");
+    		return false;
+    	}
+    	
+    	if(checkPerms(sender, args[0]))
+    	{
+    		// Load a schematic
+    		if(args[0].equalsIgnoreCase("load"))
+    		{
+    			if(args.length < 6)
+    			{
+    				sender.sendMessage(ChatColor.GRAY + "- " + ChatColor.RED + "/schematic load <filename> <world> <x> <y> <z>");
+    				return false;
+    			}
+    			
+    			return true;
+    		}
     		
+    		// Reload the config
+    		else if(args[0].equalsIgnoreCase("reload"))
+    		{
+    			return true;
+    		}
+    		
+    		// View help menu
+    		else if(args[0].equalsIgnoreCase("help"))
+    			return printHelpMenu(sender, args[1]);
+    		
+    		// Unexpected argument
+    		else
+    			sender.sendMessage("Command not found");
+    			
     	}
     	return false;
 	}
+    
+    /*
+     * Displays the help menu
+     */
+    public boolean printHelpMenu(CommandSender sender, String pageStr)
+    {
+    	int page = 1;
+    	
+    	// If user specifies a page number,
+    	// otherwise page = 1;
+    	if(isNullEmpty(pageStr))
+    		page = Integer.parseInt(pageStr);
+    	
+    	// Send header message
+    	sender.sendMessage(ChatColor.GRAY + "-------- [ " + ChatColor.GOLD + "SchematicUtility" + ChatColor.GRAY + " ] --------\n"+
+    			ChatColor.AQUA + "Help Page " + ChatColor.GOLD + page + "/1\n");
+    	
+    	// Help Page 1
+    	if(page == 1)
+    	{
+    		sender.sendMessage(
+    		ChatColor.GREEN + "/schematic help" + ChatColor.RED + " <page>\n" +
+    			ChatColor.GRAY + "Shows the help page.\n" +
+    				
+    		ChatColor.GREEN + "/schematic load " + ChatColor.RED + "<filename> <world> <x> <y> <z>\n" +
+				ChatColor.GRAY + "Load a schematic.\n" +
+    		
+			ChatColor.GREEN + "/schematic reload\n" +
+				ChatColor.GRAY + "Reloads the config.\n"
+			);
+    	}
+    	return true;
+    }
 }
