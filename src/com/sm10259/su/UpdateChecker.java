3ea0d9e846b92ade778be497e5f5b8b194e3b4b9
Sarah Myerson <myersonsarah1999@gmail.com>
2022-01-28 13:26:02 -0500

    Created update checker

--- src/com/sm10259/su/UpdateChecker.java
+++ src/com/sm10259/su/UpdateChecker.java
@@ -1,6 +1,29 @@
 package com.sm10259.su;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class UpdateChecker
 {
+	private final int resourceId;
 	
+	public UpdateChecker(int resourceId)
+	{
+		this.resourceId = resourceId;
+    }
+	
+	public int getVersion() throws IOException
+	{
+		URL url = new URL("https://api.spigotmc.org/legacy/update.php?resource="+ resourceId);
+		
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+		    for (String line; (line = reader.readLine()) != null;)
+		    {
+		        return Integer.parseInt(line);
+		    }
+		}
+		return -1;
+	}
 }
